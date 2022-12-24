c651282ca1c5746bf3a238e48dbc3620a878dea2
Gal Lisha <digitalgal.info@gmail.com>
2022-12-24 15:21:51 +0200

    waiting for merge on master

--- src/gittest1/Main.java
+++ src/gittest1/Main.java
@@ -3,5 +3,7 @@ package gittest1;
 public class Main {
     public static void main(String[]args) {
     	System.out.println("new test");
+    	
+    	System.out.println("waitng to merge");
     }
 }
